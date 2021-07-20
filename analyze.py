f5ef9ea1eaf51fd9c8c1832baf612f07f89a148e
pranamg <pranamg@users.noreply.github.com>
2021-07-20 23:10:15 +0530

    Updaet analyze.py

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 #data analyze
 
 from csv import reader
-from statistics import median
+from statistics import mean
 
 #Perform our analysis
 #We will find the average of our tempurature readings
@@ -12,5 +12,5 @@ with open("data.txt",'r') as data_file:
     for row in csv_reader:
         total.append(int(row[1]))
 
-print("Average tempurature readings :", median(total))
+print("Average tempurature readings :", mean(total))
 
